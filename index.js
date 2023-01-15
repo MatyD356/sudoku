24d41821a6825053359623b529207d051d7ee163
mpyszny <matyd356@gmail.com>
2023-01-16 00:07:30 +0100

    hanle input change

--- index.js
+++ index.js
@@ -18,22 +18,34 @@ function crateSudokuArr(){
 function createGrid(rowNum, colNum, gridElement, arr){
     let cellPointer = 0
     for(let i = 0; i< rowNum*colNum;i++){
+        const col = parseInt(i/colNum)
         const div = document.createElement('div')
-        const input = document.createElement('input')
-        input.value = arr[parseInt(i/colNum)][cellPointer]
-        input.pattern = /[1-9]/
-        if(cellPointer % 8 === 0) cellPointer = 0
         div.className = 'gridCell'
-        div.appendChild(input)
+
+        const input = document.createElement('input')
+        input.value = arr[col][cellPointer]
+        input.addEventListener('input',(event) => onChange(event, col, cellPointer, input))
+
+        if(cellPointer % 8 === 0) {
+            cellPointer = 0
+        }
         cellPointer++
         if(sudokuGrid){
+            div.appendChild(input)
             gridElement.appendChild(div)
         }
     }
 }
-function updateArr(arr) {
+function onChange(event, colNum, cellNum, input){
+    const parsedData = event.data ? parseInt(event.data) : null
+    const parsedValue = parseInt(event.target.value)
 
+    if(isNaN(parsedData) || parsedValue > 9 || parsedValue < 1){
+        return input.value = mainArr[colNum][cellNum]
+    }
+
+    mainArr[colNum][cellNum] = event.data
 }
 
-const arr = crateSudokuArr()
-createGrid(9,9, sudokuGrid, arr)
+const mainArr = crateSudokuArr()
+createGrid(9,9, sudokuGrid, mainArr)
